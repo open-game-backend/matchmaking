e101bdf2c99fff544dbfd52433ef0e509b20c7c4
Nick Prühs <dev@npruehs.de>
2020-12-09 18:21:56 +0100

    Use new version of util and net libraries.

--- src/main/java/de/opengamebackend/matchmaking/MatchmakingApplication.java
+++ src/main/java/de/opengamebackend/matchmaking/MatchmakingApplication.java
@@ -1,5 +1,6 @@
 package de.opengamebackend.matchmaking;
 
+import de.opengamebackend.util.config.ApplicationConfig;
 import de.opengamebackend.util.EnableOpenGameBackendUtils;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,7 +9,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableOpenGameBackendUtils
@@ -19,12 +19,10 @@ public class MatchmakingApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI(Environment environment) {
-		String applicationVersion = environment.getProperty("application.version");
-
+	public OpenAPI customOpenAPI(ApplicationConfig applicationConfig) {
 		return new OpenAPI().info(new Info()
 				.title("Open Game Backend Matchmaking")
-				.version(applicationVersion)
+				.version(applicationConfig.getVersion())
 				.description("Provides lists of available game servers, and allows players to connect to them.")
 				.license(new License().name("MIT").url("https://github.com/open-game-backend/matchmaking/blob/develop/LICENSE")));
 	}
