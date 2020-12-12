710aa604a3a58908e56cc3fcebba9332f95966c0
Nick Prühs <dev@npruehs.de>
2020-12-12 15:23:34 +0100

    Enable configuration properties scan for @ConfigurationProperties.

--- src/main/java/de/opengamebackend/matchmaking/MatchmakingApplication.java
+++ src/main/java/de/opengamebackend/matchmaking/MatchmakingApplication.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableOpenGameBackendUtils
 public class MatchmakingApplication {
 	@Bean
